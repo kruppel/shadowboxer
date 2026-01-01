d5b3478d89928c091cf8c292022a5d3fda314737
Kurt Ruppel <kurt@middesk.com>
2026-01-01 10:29:05 -0800

    chore: update text wrapping to 80 characters

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_ruby_formatter = "rubocop"
+
+vim.opt.textwidth = 80
+vim.opt.formatoptions:append("c")
